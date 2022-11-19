a145d91d856689dd979d03e071aa7e0f20d03869
Vicky1722 <vigneshvijay66@gmail.com>
2022-11-19 23:02:25 +0530

    Demo added

---
+++ Array/src/org/cts/test/SingleArray_Println.java
@@ -0,0 +1,18 @@
+package org.cts.test;
+
+public class SingleArray_Println {
+
+	public static void main(String[] args) {
+  		 
+		int a[]= new int[5];
+		
+		    a[0]=1;
+		    a[1]=2;
+	        a[2]=3;
+		    a[3]=4;
+			a[4]=5;
+			
+	   System.out.println(a[3]);
+	}
+}
+	
